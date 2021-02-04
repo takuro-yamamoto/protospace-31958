478fef431db1eaf2bee619a3999660be4911b8f9
takuro-yamamoto <taku50lax@outlook.jp>
2021-02-04 19:23:31 +0900

    コントローラーの修正

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,6 +5,7 @@ class CommentsController < ApplicationController
       redirect_to prototype_path(@comment.prototype.id)
     else
       @prototype = Prototype.find(params[:prototype_id])
+      @comments = @prototype.comments
       render "prototypes/show"
     end
   end
